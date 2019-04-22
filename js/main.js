031b2462904b12be69b4432bdcd05a3172a31049
Leo Holman (Student Employee) <lahwcs1@ad.rit.edu>
2019-04-22 17:35:33 -0400

    Add question functionality
    Needs
    -More questions
    -Advance to next question on response
    -Set question screen to next question text
    -Check if answer is good or not

---
+++ js/main.js
@@ -0,0 +1,67 @@
+//get constants
+const questionScreenText = document.getElementById("questionScreenText");
+const responseScreenText = document.getElementById("responseScreenText");
+const questions = {
+    "1" : {
+        "question" : "I'm writing a paper about computer science ethics",
+        "database" : "Good place to start looking! Try narrowing your search results to only scholarly articles."
+    }
+};
+
+//Wire up buttons
+document.getElementById("journal").addEventListener("click", () => {
+    checkAnswer("journal");
+});
+
+document.getElementById("newspaper").addEventListener("click", () => {
+    checkAnswer("newspaper");
+});
+
+document.getElementById("book").addEventListener("click", () => {
+    checkAnswer("book");
+});
+
+document.getElementById("web").addEventListener("click", () => {
+    checkAnswer("web");
+});
+
+document.getElementById("database").addEventListener("click", () => {
+    checkAnswer("database");
+});
+
+// document.getElementById("magazine").addEventListener("click", () => {
+//     checkAnswer("magazine");
+// });
+
+// document.getElementById("encyclopedia").addEventListener("click", () => {
+//     checkAnswer("encyclopedia");
+// });
+
+function setResponseScreenText(string){
+    responseScreenText.innerHTML = string;
+}
+
+function setQuestionScreenText(string){
+    questionScreenText.innerHTML = string;
+}
+
+function checkAnswer(buttonName){
+    let response = questions[thisQuestionNumber][buttonName];
+    setResponseScreenText(response);
+    document.getElementById("book").focus();
+}
+//Initalize to 0 so first question appears
+var thisQuestionNumber = 0;
+
+function nextQuestion(){
+    if(thisQuestionNumber < Object.keys(questions).length){
+        thisQuestionNumber++;
+        setQuestionScreenText(questions[thisQuestionNumber].question);
+    }
+}
+
+//set keyboard to target first button
+document.getElementById("book").focus();
+
+//Ask first question
+nextQuestion();
